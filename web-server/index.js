8944925fad2cbaf9b3072c9bfdd6e18c8e1d7cf9
Matt Williams <matwilliams@hotmail.com>
2018-02-24 16:28:34 +0000

    Add QuickBooks hooks

--- web-server/index.js
+++ web-server/index.js
@@ -1,10 +1,49 @@
 'use strict';
 
-var app = require('express')()
+const config = require('./config.json');
+const express = require('express');
+const quickbooks = require('quickbooks');
 
-app.use('/', function (req, res) {
-  res.end('Hello, World!');
-})
+const qbo = new quickbooks(config.CONSUMER_KEY,
+                           config.CONSUMER_SECRET,
+                           config.OAUTH_TOKEN,
+                           config.OAUTH_TOKEN_SECRET,
+                           config.REALM_ID,
+                           config.REFRESH_TOKEN,
+                           "2.0",
+                           true,
+                           true); // turn debugging on
+
+const app = express();
+
+app.get('/api/v1/orders', function (req, res) {
+  res.json(
+    {
+      orders: [
+        {
+          timestamp: 123456789, // ms since epoch
+          number: 12, // 01-99
+          items: [
+            {
+              quantity: 3,
+              description: "Phad Thai - Tofu",
+              ready: 3,
+            },
+            {
+              quantity: 2,
+              description: "Thai Green Curry - Chicken",
+              ready: 1,
+            },
+          ],
+          ready: true,
+          complete: false
+        }
+      ]
+    }
+  );
+});
+
+app.use('/', express.static('public'));
  
 require('letsencrypt-express').create({
   server: 'staging',
